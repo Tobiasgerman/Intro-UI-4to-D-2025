e003f447be57137e9c8c642fce4e4fe5de844a4b
TobiasGerman <tobigerman2019@gmail.com>
2025-05-05 10:16:09 -0300

    ejercicio terminado

---
+++ Assets/Clave.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clave : MonoBehaviour
+{
+    public  TMP_InputField InputClave;
+    public TextMeshProUGUI Txtresultado;
+    string clave ="tobi1";
+    string claveIngresada;
+    string mensaje;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Txtresultado.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void msj()
+    {
+        claveIngresada = InputClave.text;
+        if (claveIngresada == clave)
+        {
+            mensaje = "Clave Correcta";
+        } else if  (claveIngresada != clave)
+        {
+            mensaje = "Clave incorrecta";
+            
+        }
+        Txtresultado.text = mensaje;
+    }
+}
